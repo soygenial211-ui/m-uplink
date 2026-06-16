71d08e4ff107980940a0ec15ba131de4671b9d0c
soygenial211-ui <soygenial211@gmail.com>
2026-06-15 22:39:32 -0600

    Update limites.js

--- limites.js
+++ limites.js
@@ -1,6 +1,6 @@
 // Configuración de la fecha y hora límite de entrega
 const limiteTiempo = {
-    dia: 1,
+    dia: 16,
     mes: 6,      // 1 = Enero, 2 = Febrero, ..., 6 = Junio, 12 = Diciembre
     anio: 2026,
     hora: 11,    // Formato de 24 horas (0 a 23)
